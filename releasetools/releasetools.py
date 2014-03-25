1e4d6632c89211c5f76f07f494f209ce35e70416
hedwig34 <hedwig34@gmail.com>
2014-03-25 10:22:11 -0400

    d2lte: Clean up redundant entries in releasetools.py     and add support for other d2 devices
    Change-Id: I2ee047006f5f059d61d440451263f12461292a68

--- releasetools/releasetools.py
+++ releasetools/releasetools.py
@@ -1,19 +1,30 @@
-"""Custom OTA commands for Samsung d2 devices """
+# Copyright (C) 2012 The Android Open Source Project
+# Copyright (C) 2013 The CyanogenMod Project
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#      http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+""" Custom OTA commands for d2 devices """
 
 def FullOTA_InstallEnd(info):
-	info.script.AppendExtra('ifelse(is_substring("I747", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_att/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("I747", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_att/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530C", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530C", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530M", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530M", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("L710V", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_spr/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("L710V", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_spr/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("L710W", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_spr/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("L710W", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_spr/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("T999", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_tmo/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("T999", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_tmo/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530U", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("R530U", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/libril.so /system/lib/libril.so"));')
-	info.script.AppendExtra('ifelse(is_substring("I535", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_vzw/libril-qc-qmi-1.so /system/lib/libril-qc-qmi-1.so"));')
-	info.script.AppendExtra('ifelse(is_substring("I535", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_vzw/libril.so /system/lib/libril.so"));')
+	info.script.AppendExtra('ifelse(is_substring("I535", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_vzw/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("R530", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("L710", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_spr/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("S960L", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("S968C", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_r530/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("I747", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_att/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("T999", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_tmo/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("T699", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_tmo/* /system/lib/"));')
+	info.script.AppendExtra('ifelse(is_substring("T699", getprop("ro.bootloader")), run_program("/system/bin/wifimac.sh"));')
+	info.script.AppendExtra('ifelse(is_substring("I437", getprop("ro.bootloader")), run_program("/system/bin/wifimac.sh"));')
+	info.script.AppendExtra('ifelse(is_substring("SC06D", getprop("ro.bootloader")), run_program("/sbin/sh", "-c", "busybox mv /system/lib_tmo/* /system/lib/"));')
